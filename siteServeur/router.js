73bfaa58a1ba1a829b4ef6f6bad9860e568ad75e
Clement Larpent <clement.larpent@gmail.com>
2020-12-09 17:37:22 +0100

    suppresion de libre

--- siteServeur/router.js
+++ siteServeur/router.js
@@ -1,24 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const twig = require("twig");
-const  mongoose  = require("mongoose");
-const livreSchema= require("./models/livres.modele")
-
+const  mongoose = require("mongoose");
+const livreSchema= require("./models/livres.modele");
+const { response } = require("express");
 
 router.get("/", (requete, reponse) =>{
     reponse.render("accueil.html.twig")
 })
 
+//recuperation du livre
 router.get("/livres", (requete, reponse) =>{
     livreSchema.find()
     .exec()
     .then(livres =>{
-        console.log(livres)
-        reponse.render("livres/liste.html.twig", {liste : livres})
+        reponse.render("livres/liste.html.twig", {liste : livres,message : reponse.locals.message})
     })
     .catch();
 })
 
+// Sauvegarde d'un livre via le formulaire
 router.post("/livres", (requete, reponse) =>{
     const livre = new livreSchema({
         _id: new mongoose.Types.ObjectId(),
@@ -38,7 +39,7 @@ router.post("/livres", (requete, reponse) =>{
     })
 });
 
-
+//affichage du livre 
 router.get("/livres/:id", (requete, reponse) =>{
     livreSchema.findById(requete.params.id)
     .exec()
@@ -50,7 +51,23 @@ router.get("/livres/:id", (requete, reponse) =>{
     })
 })
 
+//suppression du livre
+router.post("/livres/delete/:id", (requete, reponse) =>{
+    livreSchema.remove({_id:requete.params.id})
+    .exec()
+    .then(resultat =>{
+        requete.session.message = {
+            type : 'success',
+            contenu : 'Suppression effectuée'
+        }
+        reponse.redirect("/livres");
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
 
+//gestion des erreurs
 router.use((requete,reponse, suite) => {
     const error = new Error("Page non trouvée");
     error.status = 404;
--- siteServeur/server.js
+++ siteServeur/server.js
@@ -4,14 +4,33 @@ const morgan = require("morgan");
 const router = require("./router");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const { populate } = require("./models/livres.modele");
 
+// gestion de la session
+server.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000}
+}))
+
+//Connexion à la base de données mongodb
 mongoose.connect("mongodb://localhost/biblio", {useNewUrlParser:true, useUnifiedTopology: true});
 
 server.use(express.static("public"));
-server.use(morgan("dev"));
+server.use(morgan("dev")); // module morgan
 server.use(bodyParser.urlencoded({extended:false})); //utilisation de body parse rpour les info poste par formulaire
+server.set('trust proxy', 1);
 
-server.use("/", router);
+//traitement de la variable de session
+server.use((requete, reponse, suite) =>{
+    reponse.locals.message = requete.session.message;
+    delete requete.session.message; //suppresion de l'information
+    suite(); //permet de poursuivre
+})
 
-server.listen(3000);
+server.use("/", router); //route de redirection
+
+server.listen(3000); //port de l'écoute (localhost: 3000)
 
